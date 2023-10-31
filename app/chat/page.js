aaa32a0f134089e45d0f638883f7e1c6ff5b3038
thePeeyush <peeyushdehariya951@gmail.com>
2023-10-31 13:05:57 +0530

    Some style changes

--- app/chat/page.js
+++ app/chat/page.js
@@ -9,7 +9,7 @@ return (
     <div className="w-full h-full overflow-y-scroll chatRoot">
        <div className="max-w-4xl mx-auto my-20">
        <MessageLeft text={"Namaste main hu aapka legal advisor, kripya batayen me apki kaise madad kar sakta hu"}/>
-       {/* <MessageRight text={"main hu nanha balak"}/>
+       <MessageRight text={"main hu nanha balak"}/>
        <MessageLeft text={"kuch nhi balak apki sahayta ke liye uplabdha hai 1098 toll free no for child helpline"}/>
        <MessageRight text={"ha kya"}/>
        <MessageLeft text={"are bhai ha main ekdam sahi keh raha hu"}/>
@@ -19,7 +19,7 @@ return (
        <MessageRight text={"kyo ai"}/>
        <MessageLeft text={"nahi mujhe koi dikkat nhi hai aapko hai to mujhe bataiye"}/>
        <MessageRight text={"bye"}/>
-       <MessageLeft text={"bye, main apki sahata ki liye hamesha aapke sath hu. "}/> */}
+       <MessageLeft text={"bye, main apki sahata ki liye hamesha aapke sath hu. "}/>
        {
         chat.map((chat)=>{
           return(chat.isBot === true ? <MessageLeft text={chat.text}/> : <MessageRight text={chat.text} />)
