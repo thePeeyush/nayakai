f055d0b314bd5476ccf244da250777e631832f8f
thePeeyush <peeyushdehariya951@gmail.com>
2024-03-02 16:12:32 +0530

    add suspence boundry in lawyers

--- app/lawyers/page.js
+++ app/lawyers/page.js
@@ -2,11 +2,11 @@ import LawyerCard from "@/components/LawyerCard";
 import Searchbox from "@/components/Searchbox";
 import getUrl from "@/utils/getUrl";
 import generateUniqueId from "@/utils/newid";
+import { Suspense } from "react";
 
 async function getLawyers(searchParams) {
 
-  const curUrl = getUrl() ;
-
+  const curUrl = getUrl();
   let url = '';
   if (searchParams.search) {
     url = `${curUrl}/api/lawyers?search=${searchParams.search}`
@@ -26,6 +26,7 @@ export default async function Page({ searchParams }) {
 
     <div className="p-4 lg:px-16 mt-16 overflow-y-scroll w-full">
       <Searchbox text={"Lawyers"} />
+      <Suspense fallback={<Loading/>}>
       {Lawyers.message === 'Error' ? <h1>! Failed to fetch data</h1> : (
         <div className="w-full flex flex-wrap gap-2 lg:gap-6 mb-20">
           {Lawyers.result.length < 1 ? <h1 className="text-center lg:text-left lg:pt-20 lg:pl-40 text-red-400 w-full pt-[30vh]">! Not Found</h1> : Lawyers.result.sort((a, b) => b.rating - a.rating).map((lawyer) => {
@@ -35,7 +36,13 @@ export default async function Page({ searchParams }) {
           })}
         </div>
       )}
-
+      </Suspense>
     </div>
+  )
+}
+
+function Loading() {
+  return(
+    <h1>🌀Loading...</h1>
   )
 }
